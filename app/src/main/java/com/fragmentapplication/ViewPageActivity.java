c4a425b3a60557efbbe851ca7d0d678ca4e34c24
sanam <thapasanam750@gmail.com>
2019-04-24 21:34:40 +0545

    fragment 2

--- app/src/main/java/com/fragmentapplication/ViewPageActivity.java
+++ app/src/main/java/com/fragmentapplication/ViewPageActivity.java
@@ -1,14 +1,34 @@
 package com.fragmentapplication;
 
+import android.support.design.widget.TabLayout;
+
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import adopter.ViewPagerAdopter;
+import fragment.FirstFragment;
+
 public class ViewPageActivity extends AppCompatActivity {
+
+private ViewPager viewPager;
+private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_page);
+
+tabLayout=findViewById(R.id.tabId);
+viewPager-findViewById(R.id.viewPager);
+
+
+        ViewPagerAdopter adopter=new ViewPagerAdopter(getSupportFragmentManager());
+        adopter.addFragment(new FirstFragment(),"sum");
+        adopter.addFragment(new FirstFragment(),"area of Circle");
+
+        viewPager.setAdapter(adopter);
+        tabLayout.setupWithViewPager(viewPager);
 
 
     }
--- app/src/main/java/fragment/SecondFragment.java
+++ app/src/main/java/fragment/SecondFragment.java
@@ -6,14 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.fragmentapplication.R;
+
+import java.util.zip.Inflater;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SecondFragment extends Fragment {
+public class SecondFragment extends Fragment implements View.OnClickListener{
 
+private Button BtnCalarea;
+private EditText etthird;
 
     public SecondFragment() {
         // Required empty public constructor
@@ -24,7 +31,21 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second2, container, false);
+     View view=inflater.inflate(R.layout.fragment_second2,container,false);
+
+
+        etthird=view.findViewById(R.id.etthird);
+        BtnCalarea=view.findViewById(R.id.btncalarea);
+
+        BtnCalarea.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+float radius=Float.parseFloat(etthird.getText().toString());
+float area=3.14f * radius*radius;
+
+        Toast.makeText(getActivity(),"area of circle:" + area,Toast.LENGTH_LONG).show();
+    }
 }
