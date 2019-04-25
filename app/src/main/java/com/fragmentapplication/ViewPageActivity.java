1c50514b02c2ade771fdec5963bf213bc7bb3bd4
sanam <thapasanam750@gmail.com>
2019-04-25 12:26:05 +0545

    adding more fragment

--- app/src/main/java/com/fragmentapplication/ViewPageActivity.java
+++ app/src/main/java/com/fragmentapplication/ViewPageActivity.java
@@ -7,7 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import adopter.ViewPagerAdopter;
+import fragment.Celsius_Fragment;
 import fragment.FirstFragment;
+import fragment.SI_Fragment;
+import fragment.SecondFragment;
 
 public class ViewPageActivity extends AppCompatActivity {
 
@@ -20,12 +23,15 @@ private TabLayout tabLayout;
         setContentView(R.layout.activity_view_page);
 
 tabLayout=findViewById(R.id.tabId);
-viewPager-findViewById(R.id.viewPager);
+viewPager=findViewById(R.id.viewPager);
 
 
         ViewPagerAdopter adopter=new ViewPagerAdopter(getSupportFragmentManager());
         adopter.addFragment(new FirstFragment(),"sum");
-        adopter.addFragment(new FirstFragment(),"area of Circle");
+        adopter.addFragment(new SecondFragment(),"area of Circle");
+        adopter.addFragment(new SI_Fragment(),"Simple Interest");
+        adopter.addFragment(new Celsius_Fragment(),"Celsius");
+
 
         viewPager.setAdapter(adopter);
         tabLayout.setupWithViewPager(viewPager);
